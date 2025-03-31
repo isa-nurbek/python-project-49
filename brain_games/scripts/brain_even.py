c864562a3114ce7d4ae5ed38158981bd961c945b
isa-nurbek <isradin.110593@gmail.com>
2025-03-31 23:58:27 +0300

    Entry point script for the Even Number game

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,34 +1,9 @@
-from random import randint
-
-import prompt
-
-from cli import welcome
-
-
-def is_even(num):
-    return num % 2 == 0
+from brain_games.games import even
+from games_logic import init_game
 
 
 def main():
-    name = welcome()
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-
-    for _ in range(3):
-        number = randint(1, 10)
-        print(f"Question: {number}")
-        answer = prompt.string('Your answer: ')
-        correct_answer = 'yes' if is_even(number) else 'no'
-
-        if answer == correct_answer:
-            print('Correct!')
-        else:
-            print(
-                f"'{answer}' is wrong answer ;(. "
-                f"Correct answer was '{correct_answer}'.\n"
-                f"Let's try again, {name}!"
-            )
-            return
-    print(f'Congratulations, {name}!')
+    init_game(even)
 
 
 if __name__ == "__main__":
