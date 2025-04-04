82f2eb2ecc84f9e38ab1a9c1140115f783ce4fa5
isa-nurbek <isradin.110593@gmail.com>
2025-04-04 20:33:09 +0300

    Implement Progression entry point script

---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,10 @@
+from brain_games.games import arithmetic_progression
+from games_logic import init_game
+
+
+def main():
+    init_game(arithmetic_progression)
+
+
+if __name__ == "__main__":
+    main()
