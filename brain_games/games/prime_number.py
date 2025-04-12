8340902e4f45e1460829386ffeb941fb58cb7a94
isa-nurbek <isradin.110593@gmail.com>
2025-04-12 03:21:29 +0300

    Fix is_prime() function

--- brain_games/games/prime_number.py
+++ brain_games/games/prime_number.py
@@ -1,20 +1,12 @@
-import math
 from random import randint
 
 GAME_RULES = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def is_prime(number):
-    if number <= 1:
+    if number < 2:
         return False
-
-    if number == 2:
-        return True
-
-    if number % 2 == 0:
-        return False
-
-    for i in range(3, int(math.sqrt(number)) + 1, 2):
+    for i in range(2, int(number**0.5) + 1):
         if number % i == 0:
             return False
 
