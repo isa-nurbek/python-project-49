9fa00c2d007e8469893fa95f447823b4b267e5a9
isa-nurbek <isradin.110593@gmail.com>
2025-04-12 03:22:16 +0300

    Beautify code in is_prime() function

--- brain_games/games/prime_number.py
+++ brain_games/games/prime_number.py
@@ -6,6 +6,7 @@ GAME_RULES = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 def is_prime(number):
     if number < 2:
         return False
+
     for i in range(2, int(number**0.5) + 1):
         if number % i == 0:
             return False
