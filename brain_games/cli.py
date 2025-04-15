fe47200a10e695077306eddbb8e80a45750b6418
isa-nurbek <isradin.110593@gmail.com>
2025-04-15 04:06:48 +0300

    Fix welcome() function logic

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -2,8 +2,5 @@ import prompt
 
 
 def welcome():
-    print("Welcome to the Brain Games!")
-    ask_name = prompt.string("May I have your name? ")
-    
-    print(f"Hello, {ask_name}!")
-    return ask_name
+    name = prompt.string("Welcome to the Brain Games!\nMay I have your name? ")
+    print(f"Hello, {name}!")
