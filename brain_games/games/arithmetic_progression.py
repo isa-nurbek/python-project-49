60f0dcddfea30bce5bc9006d3abbbcc8272545be
isa-nurbek <isradin.110593@gmail.com>
2025-04-15 04:31:19 +0300

    Refactor: simplify range() function

--- brain_games/games/arithmetic_progression.py
+++ brain_games/games/arithmetic_progression.py
@@ -4,21 +4,15 @@ GAME_RULES = "What number is missing in the progression?"
 
 
 def produce_question_and_answer():
-    progression = []
-
     progression_length = 10
     start = randint(1, 10)
     step = randint(1, 10)
-
-    for _ in range(progression_length):
-        progression.append(start)
-        start += step
+    progression = list(range(start, start + step * progression_length, step))
 
     hidden_position = randint(0, len(progression) - 1)
     correct_answer = progression[hidden_position]
 
     progression[hidden_position] = ".."
-    progression = " ".join([str(item) for item in progression])
-    question = f"{progression}"
+    question = " ".join(map(str, progression))
 
     return question, str(correct_answer)
